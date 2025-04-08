dfbcf5125495fb8a3fd2b612377a59c4eb495947
tanishq <tanishqtote.55@gmail.com>
2025-04-09 00:58:57 +0530

    Initial code added

---
+++ calculator.c
@@ -0,0 +1,41 @@
+// calculator.c (Decimal calculator with exponentiation)
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter first operand: ");
+    scanf("%lf", &num1);
+    printf("Enter operator: ");
+    scanf(" %c", &operator);
+    printf("Enter second operand: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
+        case '^':
+            printf("%.2lf ^ %.2lf = %.2lf\n", num1, num2, pow(num1, num2));
+            break;
+        default:
+            printf("Error: Unsupported operator.\n");
+    }
+
+    return 0;
+}
